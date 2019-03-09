abe242567fc639697698323a431976343e88afaf
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-09 16:14:54 -0700

    Added implementation of SearchBar using semantic-ui

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Segment, Form } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+
+class SearchBar extends Component {
+  state = { term: "" };
+
+  render() {
+    return (
+      <Segment>
+        <Form onSubmit={this.onFormSubmit}>
+          <Form.Field>
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </Form.Field>
+        </Form>
+      </Segment>
+    );
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+}
+
+export default SearchBar;
